dc217d7583941a2b8df4274632a3625e0452728f
nishan das <nishandas880@gmail.com>
2026-04-16 11:22:07 +0600

    home page is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,5 +24,54 @@
             marginTop: ''
         });
     });
+     // OverlayScrollbars
+        const {
+            OverlayScrollbars,
+            ClickScrollPlugin
+        } = OverlayScrollbarsGlobal;
+        // Initialize the ClickScrollPlugin
+        OverlayScrollbars.plugin(ClickScrollPlugin);
+        $("body").each(function () {
+            OverlayScrollbars(this, {
+                scrollbars: {
+                    clickScroll: true,
+                    autoHide: "leave",
+                    dragScrolling: true,
+                    clickScrolling: true,
+                },
+                scrollBehavior: 'smooth',
+            });
+        });
+
+        // lenis
+        // Initialize a new Lenis instance for smooth scrolling
+        const lenis = new Lenis();
+
+        // Listen for the 'scroll' event and log the event data to the console
+        // lenis.on('scroll', (e) => {
+        //     console.log(e);
+        // });
+
+        // Synchronize Lenis scrolling with GSAP's ScrollTrigger plugin
+        lenis.on('scroll', ScrollTrigger.update);
+
+        // Add Lenis's requestAnimationFrame (raf) method to GSAP's ticker
+        // This ensures Lenis's smooth scroll animation updates on each GSAP tick
+        gsap.ticker.add((time) => {
+            lenis.raf(time * 1000); // Convert time from seconds to milliseconds
+        });
+
+        // Disable lag smoothing in GSAP to prevent any delay in scroll animations
+        gsap.ticker.lagSmoothing(0);
+        // lenis
+
+        // Header scroll detection - add/remove active class
+        lenis.on('scroll', (e) => {
+            if (e.scroll > 0) {
+                $('.header-area').addClass('active');
+            } else {
+                $('.header-area').removeClass('active');
+            }
+        });
 
 })(jQuery);
